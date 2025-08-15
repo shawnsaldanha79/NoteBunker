efdca442564f7389297c1d6af565d9a094839847
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-08-15 11:41:00 +0530

    Initial commit

---
+++ src/main/java/com/secure/notes/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.secure.notes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "hello");
+        return map;
+    }
+    @GetMapping("/hi")
+    public List<String> hi() {
+        List<String> list = new ArrayList<>();
+        list.add("Hi");
+        return list;
+    }
+}
