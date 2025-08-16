c2f5617a9a68dafaf19a0c88a4b314d181827290
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-08-16 10:56:07 +0530

    Update SecurityConfig - Add database authentication

--- src/main/java/com/secure/notebunker/security/SecurityConfig.java
+++ src/main/java/com/secure/notebunker/security/SecurityConfig.java
@@ -6,7 +6,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +27,27 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}admin")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 }
