639d22d72cc377ac5211adb832c4c7e0da910683
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-08-20 14:12:23 +0530

    Update AuthController and UserRepository

--- src/main/java/com/secure/notebunker/controller/AuthController.java
+++ src/main/java/com/secure/notebunker/controller/AuthController.java
@@ -1,8 +1,16 @@
 package com.secure.notebunker.controller;
 
+import com.secure.notebunker.model.AppRole;
+import com.secure.notebunker.model.Role;
+import com.secure.notebunker.model.User;
+import com.secure.notebunker.repository.RoleRepository;
+import com.secure.notebunker.repository.UserRepository;
 import com.secure.notebunker.security.jwt.JwtUtils;
 import com.secure.notebunker.security.request.LoginRequest;
+import com.secure.notebunker.security.request.SignupRequest;
 import com.secure.notebunker.security.response.LoginResponse;
+import com.secure.notebunker.security.response.MessageResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +20,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -28,11 +39,23 @@ public class AuthController {
 
     private final JwtUtils jwtUtils;
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthController(JwtUtils jwtUtils, AuthenticationManager authenticationManager) {
+    public AuthController(
+            JwtUtils jwtUtils,
+            AuthenticationManager authenticationManager,
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder
+    ) {
         this.jwtUtils = jwtUtils;
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/public/signin")
@@ -57,5 +80,55 @@ public class AuthController {
                 .collect(Collectors.toList());
         LoginResponse response = new LoginResponse(userDetails.getUsername(), roles, jwtToken);
         return new ResponseEntity<Object>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/public/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+        if (userRepository.existsByUserName(signupRequest.getUsername())) {
+            return new ResponseEntity<Object>(
+                    new MessageResponse("Error: Username is already taken!"), HttpStatus.BAD_REQUEST
+            );
+        }
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            return new ResponseEntity<Object>(
+                    new MessageResponse("Error: Email is already in use!"), HttpStatus.BAD_REQUEST
+            );
+        }
+        User user = new User(
+                signupRequest.getUsername(),
+                signupRequest.getEmail(),
+                passwordEncoder.encode(signupRequest.getPassword())
+        );
+        Set<String> strRoles = signupRequest.getRole();
+        Role role = null;
+        if (strRoles == null || strRoles.isEmpty()) {
+            roleRepository.findByRoleName(AppRole.ROLE_USER).orElseThrow(() ->
+                new RuntimeException("Error: Role not found!")
+            );
+        } else {
+            String roleStr = strRoles.iterator().next();
+            if (roleStr.equals("admin")) {
+                role = roleRepository.findByRoleName(AppRole.ROLE_ADMIN).orElseThrow(() ->
+                        new RuntimeException("Error: Role not found!")
+                );
+            } else {
+                role = roleRepository.findByRoleName(AppRole.ROLE_USER).orElseThrow(() ->
+                        new RuntimeException("Error: Role not found!")
+                );
+            }
+            user.setAccountNonLocked(true);
+            user.setAccountNonExpired(true);
+            user.setCredentialsNonExpired(true);
+            user.setEnabled(true);
+            user.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+            user.setAccountExpiryDate(LocalDate.now().plusYears(1));
+            user.setTwoFactorEnabled(false);
+            user.setSignUpMethod("email");
+        }
+        user.setRole(role);
+        userRepository.save(user);
+        return new ResponseEntity<Object>(
+                new MessageResponse("User registered successfully"), HttpStatus.OK
+        );
     }
 }
