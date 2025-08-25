3edacd16043691af7745ef84ffd360b15802f1a0
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-08-25 10:08:11 +0530

    Update api.js, ContextApi.jsx and App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
 
 const App = () => {
     return (
         <Router>
             <Navbar />
+            <Toaster position="bottom-center" reverseOrder={false} />
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
             <Footer />
         </Router>
     );
