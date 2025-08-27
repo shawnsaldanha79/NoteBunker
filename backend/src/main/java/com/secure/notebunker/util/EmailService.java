ab58ac9cf340e67128822a36ee7cad63d81aff9b
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-08-27 17:02:18 +0530

    Add PasswordResetToken, PasswordResetTokenRepository and EmailService files

---
+++ backend/src/main/java/com/secure/notebunker/util/EmailService.java
@@ -0,0 +1,27 @@
+package com.secure.notebunker.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendPasswordResetEmail(String to, String resetUrl) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Password Reset Request");
+        message.setText("Click the link to reset your password: " + resetUrl);
+        mailSender.send(message);
+    }
+
+
+}
